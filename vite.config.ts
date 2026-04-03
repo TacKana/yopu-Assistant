9c49fc360971a864e701dd85bdd7f16a00160392
TacKana <1@kiik.top>
2026-04-04 01:05:35 +0800

    chore: 更新版本号至 0.0.3
    更新 package.json 和 vite.config.ts 中的版本号，以准备新版本的发布。

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
         namespace: 'yopu-assistant',
         match: ['https://yopu.co/view/*'],
         name: "有谱么助手",
-        version: '0.0.2',
+        version: '0.0.3',
         author: "Neo",
         copyright: "2026 Neo MIT 协议开源",
         description: "有谱么助手，为 有谱么 添加一些功能，如全屏看谱，防止息屏。",
